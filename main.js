8a4f1a9f7909d99dc86b0ac9422f689882232878
phenix1229 <paul.garay@codeimmersives.com>
2019-11-11 17:58:06 -0500

    finished index and experience pages

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
-// function addLinkToThumb(img){
-//     const iLink = document.createElement('a');
-//     iLink.target = '_blank'
-//     iLink.href = document.querySelector(img).src; 
-//     return document.querySelector(img).appendChild(iLink);
-// }
-
-// 
-
 document.querySelectorAll('.thumbNail').forEach(function (pic){
     const thing = document.querySelector('ul');
-    // console.log(thing)
-    // const picInfo = document.querySelector('.img').innerHTML;
-    // console.log(picInfo);
-    // const pic = document.querySelector('.img > img');
     const iLink = document.createElement('a');
-    // const picThumb = document.createElement('img')
     iLink.target = '_blank';
     iLink.href = pic.src; 
-    // picThumb.src = picInfo
     iLink.innerHTML = pic.outerHTML
-    // thing.removeChild(pic);
     thing.replaceChild(iLink, pic);
-    // thing.appendChild(iLink);
 })
